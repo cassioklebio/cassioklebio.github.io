829d17b46c420bd1f4ab2424381727e93f44bbe9
cassioklebio <cassiotr2009@gmail.com>
2017-03-11 15:53:21 -0300

    initial commit

---
+++ auladehtml/js/Script.js
@@ -0,0 +1,9 @@
+
+var pessoa = {
+	nome: 'Cássio Klebio Pereira Lins',
+	cargo: 'Desenvolvedor de Software',
+	foto: 'img/perfil.jpg'
+}
+document.getElementById('nome-cabecalho').innerHTML = pessoa.nome;
+document.getElementById('cargo-cabecalho').innerHTML = pessoa.cargo;
+document.getElementById('foto-cabecalho').innerHTML = pessoa.foto;
